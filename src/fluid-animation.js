42bf924bf29e1334d31e40e742be397394d98325
ShadowLogan31 <48600104+DevLogOfficial@users.noreply.github.com>
2023-05-28 23:18:54 -0500

    Update fluid-animation.js

--- src/fluid-animation.js
+++ src/fluid-animation.js
@@ -75,7 +75,7 @@ export default class FluidAnimation {
     var g = color[1];
     var b = color[2];
     var a = color[3];
-    this._canvas.style.background = (r, g, b, a);
+    this._gl.fillStyle = 'red';
   }
 
   addSplat(splat) {
